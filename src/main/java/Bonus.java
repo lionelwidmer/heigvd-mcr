3be2699bd5a94494647920c38fc705f83e5090ae
LESRP\cje <j.corbaz@retraitespopulaires.ch>
2019-05-22 16:03:33 +0200

    added main files for game

---
+++ src/main/java/Bonus.java
@@ -0,0 +1,19 @@
+import Player.PowerUp;
+
+import java.util.ArrayList;
+
+public class Bonus {
+    PowerUp pwu;
+    double posX;
+    double posY;
+
+    Bonus(PowerUp power) {
+        this.pwu = power;
+    }
+
+    void move() {
+    }
+
+    void draw() {
+    }
+}
---
+++ src/main/java/Breakout.java
@@ -0,0 +1,12 @@
+import Brick.AbstractBrick;
+import Player.AbstractBar;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class Breakout {
+    JFrame window;
+    ArrayList<Bonus> bonuses;
+    ArrayList<AbstractBrick> bricks;
+    AbstractBar player;
+}
